5de094207402d22fd7db221459604dd28ed47414
Siddharth S Desai <101576956+siddharthdesai06@users.noreply.github.com>
2024-01-15 12:49:42 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+import streamlit as st
+
+import langchain_helper
+
+st.title("Restaurant Name generator")
+
+cuisine = st.sidebar.selectbox("Pick a Cuisine",("Indian","Italian","Mexican","Arabic"))
+
+
+if cuisine:
+    response = langchain_helper.generate_restaurant_name(cuisine)
+
+    st.header(response['restaurant_name'].strip())
+
+    menu_items = response['menu_items'].strip().split(",")
+    st.write("**Menu Items**")
+
+    for item in menu_items:
+        st.write("-",item)
